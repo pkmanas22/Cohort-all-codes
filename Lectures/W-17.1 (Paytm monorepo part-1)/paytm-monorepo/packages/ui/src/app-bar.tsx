b3bd45ac9d3c5614db4e2aa62f5f15e82028a672
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-05-03 21:31:14 +0530

    app bar fixes

--- Lectures/W-17.1 (Paytm monorepo part-1)/paytm-monorepo/packages/ui/src/app-bar.tsx
+++ Lectures/W-17.1 (Paytm monorepo part-1)/paytm-monorepo/packages/ui/src/app-bar.tsx
@@ -9,15 +9,17 @@ interface AppBarProps {
   onsignout: any,
 }
 export const AppBar = ({ user, onsignin, onsignout }: AppBarProps) => {
+  const isLoading = user === undefined;
 
   return <div className="flex justify-between border-b px-4">
     <div className="text-lg flex flex-col justify-center">
       PayTM
     </div>
-    <div className="flex flex-col justify-center pt-2">
-      <Button onclick={user ? onsignout : onsignin}>
-        {user ? "Logout" : "Login"}
-      </Button>
-    </div>
+    {!isLoading &&
+      <div className="flex flex-col justify-center pt-2">
+        <Button onclick={user ? onsignout : onsignin}>
+          {user ? "Logout" : "Login"}
+        </Button>
+      </div>}
   </div>
 };
